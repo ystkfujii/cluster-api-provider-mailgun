28ed04f35826c70651482af2ff931e5ae91e601c
ystkfujii <ystk.fujii0731@gmail.com>
2023-04-15 17:53:42 +0900

    feat: add rbac

--- controllers/mailguncluster_controller.go
+++ controllers/mailguncluster_controller.go
@@ -18,24 +18,33 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	"github.com/mailgun/mailgun-go"
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/patch"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	infrastructurev1beta1 "github.com/ystkfujii/cluster-api-provider-mailgun/api/v1beta1"
+	"github.com/go-logr/logr"
+	infrav1 "github.com/ystkfujii/cluster-api-provider-mailgun/api/v1beta1"
 )
 
 // MailgunClusterReconciler reconciles a MailgunCluster object
 type MailgunClusterReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Log       logr.Logger
+	Mailgun   mailgun.Mailgun
+	Recipient string
 }
 
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -49,7 +58,45 @@ type MailgunClusterReconciler struct {
 func (r *MailgunClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var mgCluster infrav1.MailgunCluster
+	if err := r.Get(ctx, req.NamespacedName, &mgCluster); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, mgCluster.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if cluster == nil {
+		return ctrl.Result{}, fmt.Errorf("failed to retrieve cluster: %+v", mgCluster.ObjectMeta)
+	}
+
+	if mgCluster.Status.MessageID != nil {
+		// We already sent a message, so skip reconcilation
+		return ctrl.Result{}, nil
+	}
+
+	subject := fmt.Sprintf("[%s] New Cluster %s requested", mgCluster.Spec.Priority, cluster.Name)
+	body := fmt.Sprintf("Hello! One cluster please.\n\n%s\n", mgCluster.Spec.Request)
+
+	msg := mailgun.NewMessage(mgCluster.Spec.Requester, subject, body, r.Recipient)
+	_, msgID, err := r.Mailgun.Send(msg)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	helper, err := patch.NewHelper(&mgCluster, r.Client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	mgCluster.Status.MessageID = &msgID
+	if err := helper.Patch(ctx, &mgCluster); err != nil {
+		return ctrl.Result{}, errors.Wrapf(err, "couldn't patch cluster %q", mgCluster.Name)
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -57,6 +104,6 @@ func (r *MailgunClusterReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 // SetupWithManager sets up the controller with the Manager.
 func (r *MailgunClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&infrastructurev1beta1.MailgunCluster{}).
+		For(&infrav1.MailgunCluster{}).
 		Complete(r)
 }
--- main.go
+++ main.go
@@ -31,6 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/mailgun/mailgun-go"
 	infrastructurev1beta1 "github.com/ystkfujii/cluster-api-provider-mailgun/api/v1beta1"
 	"github.com/ystkfujii/cluster-api-provider-mailgun/controllers"
 	//+kubebuilder:scaffold:imports
@@ -89,13 +90,36 @@ func main() {
 		os.Exit(1)
 	}
 
+	domain := os.Getenv("MAILGUN_DOMAIN")
+	if domain == "" {
+		setupLog.Info("missing required env MAILGUN_DOMAIN")
+		os.Exit(1)
+	}
+
+	apiKey := os.Getenv("MAILGUN_API_KEY")
+	if apiKey == "" {
+		setupLog.Info("missing required env MAILGUN_API_KEY")
+		os.Exit(1)
+	}
+
+	recipient := os.Getenv("MAIL_RECIPIENT")
+	if recipient == "" {
+		setupLog.Info("missing required env MAIL_RECIPIENT")
+		os.Exit(1)
+	}
+
+	mg := mailgun.NewMailgun(domain, apiKey)
+
 	if err = (&controllers.MailgunClusterReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:    mgr.GetClient(),
+		Log:       ctrl.Log.WithName("controllers").WithName("MailgunCluster"),
+		Mailgun:   mg,
+		Recipient: recipient,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "MailgunCluster")
 		os.Exit(1)
 	}
+
 	if err = (&controllers.MailgunMachineReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
